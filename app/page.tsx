6c11eb79e570cc3b03bf345e60868ae8b7396208
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-29 17:20:01 +0200

    f

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 
-function connectToMetaMask() {
-  if (typeof (window as any).ethereum !== 'undefined') {
-    (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-  } else {
-    alert('MetaMask is not installed. Please install it to continue.');
+function ConnectButton() {
+  function connectToMetaMask() {
+    if (typeof (window as any).ethereum !== 'undefined') {
+      (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+    } else {
+      alert('MetaMask is not installed. Please install it to continue.');
+    }
   }
+
+  return (
+    <button 
+      onClick={connectToMetaMask}
+      style={{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        backgroundColor: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer'
+      }}
+    >
+      Connect Wallet
+    </button>
+  );
 }
 
 export default function Page() {
   return (
     <div style={{ backgroundColor: 'black', height: '100vh', position: 'relative' }}>
-      <button 
-        onClick={connectToMetaMask}
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          backgroundColor: 'white',
-          padding: '10px 20px',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer'
-        }}
-      >
-        Connect Wallet
-      </button>
+      <ConnectButton />
     </div>
   );
 }
