afac6db88c02d922d7556e0725ee4a8476494745
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-29 17:22:56 +0200

    fcv

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,44 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-
-function ConnectButtonComponent() {
-  function connectToMetaMask() {
-    if (typeof (window as any).ethereum !== 'undefined') {
-      (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-    } else {
-      alert('MetaMask is not installed. Please install it to continue.');
-    }
-  }
-
-  return (
-    <button 
-      onClick={connectToMetaMask}
-      style={{
-        position: 'absolute',
-        top: '10px',
-        right: '10px',
-        backgroundColor: 'white',
-        padding: '10px 20px',
-        border: 'none',
-        borderRadius: '5px',
-        cursor: 'pointer'
-      }}
-    >
-      Connect Wallet
-    </button>
-  );
-}
-
-const ConnectButton = dynamic(() => Promise.resolve(ConnectButtonComponent), {
-  ssr: false
-});
+import React, { useEffect } from 'react';
 
 export default function Page() {
+  useEffect(() => {
+    function connectToMetaMask() {
+      if (typeof (window as any).ethereum !== 'undefined') {
+        (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+      } else {
+        alert('MetaMask is not installed. Please install it to continue.');
+      }
+    }
+
+    const button = document.getElementById('connectButton');
+    if (button) {
+      button.addEventListener('click', connectToMetaMask);
+    }
+
+    return () => {
+      if (button) {
+        button.removeEventListener('click', connectToMetaMask);
+      }
+    };
+  }, []);
+
   return (
     <div style={{ backgroundColor: 'black', height: '100vh', position: 'relative' }}>
-      <ConnectButton />
+      <button 
+        id="connectButton"
+        style={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+          backgroundColor: 'white',
+          padding: '10px 20px',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }}
+      >
+        Connect Wallet
+      </button>
     </div>
   );
 }
