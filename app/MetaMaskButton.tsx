561c74e44a36c55a1c411b49a18b501a1c3a7ebf
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-29 17:26:33 +0200

    vcx

---
+++ app/MetaMaskButton.tsx
@@ -0,0 +1,43 @@
+// MetaMaskButton.tsx
+import React, { useEffect } from 'react';
+
+export default function MetaMaskButton() {
+  useEffect(() => {
+    function connectToMetaMask() {
+      if (typeof (window as any).ethereum !== 'undefined') {
+        (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+      } else {
+        alert('MetaMask is not installed. Please install it to continue.');
+      }
+    }
+
+    const button = document.getElementById('connectButton');
+    if (button) {
+      button.addEventListener('click', connectToMetaMask);
+    }
+
+    return () => {
+      if (button) {
+        button.removeEventListener('click', connectToMetaMask);
+      }
+    };
+  }, []);
+
+  return (
+    <button 
+      id="connectButton"
+      style={{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        backgroundColor: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer'
+      }}
+    >
+      Connect Wallet
+    </button>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,11 @@
-import React, { useEffect } from 'react';
-
-use client;
+// page.tsx
+import React from 'react';
+import MetaMaskButton from './MetaMaskButton';
 
 export default function Page() {
-  useEffect(() => {
-    function connectToMetaMask() {
-      if (typeof (window as any).ethereum !== 'undefined') {
-        (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-      } else {
-        alert('MetaMask is not installed. Please install it to continue.');
-      }
-    }
-
-    const button = document.getElementById('connectButton');
-    if (button) {
-      button.addEventListener('click', connectToMetaMask);
-    }
-
-    return () => {
-      if (button) {
-        button.removeEventListener('click', connectToMetaMask);
-      }
-    };
-  }, []);
-
   return (
     <div style={{ backgroundColor: 'black', height: '100vh', position: 'relative' }}>
-      <button 
-        id="connectButton"
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          backgroundColor: 'white',
-          padding: '10px 20px',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer'
-        }}
-      >
-        Connect Wallet
-      </button>
+      <MetaMaskButton />
     </div>
   );
 }
