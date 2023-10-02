c57ef5ae2881198b8855eb43a202c0bc2fc6d142
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-10-02 14:28:45 +0200

    test swap

--- pages/index.js
+++ pages/index.js
@@ -35,15 +35,24 @@ function Home() {
 
   return (
     <RootLayout>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Crypto Guys -- &nbsp;
-            <button onClick={connectToMetaMask} className="font-mono font-bold">Connecter MetaMask</button>
-          </p>
-        </div>
-      </main>
-    </RootLayout>
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        Welcome to the Crypto Guys app, please &nbsp;
+        <button onClick={connectToMetaMask} className="font-mono font-bold">Connect MetaMask</button>
+      </p>
+    </div>
+    <div className="z-10 max-w-md w-full p-6 mt-10 bg-white rounded-xl shadow-md flex flex-col items-center">
+      <h2 className="text-2xl font-bold mb-4">USDC to GDAO Swap</h2>
+      <p className="text-sm mb-4">Here you can convert your USDC to our newly minted GDAO token.</p>
+      <div className="flex flex-col w-full mb-4">
+        <label className="mb-2">Amount of USDC:</label>
+        <input type="number" className="p-2 border rounded" placeholder="Enter amount" />
+      </div>
+      <button className="p-2 bg-blue-500 text-white rounded">Swap to GDAO</button>
+    </div>
+  </main>
+</RootLayout>
   );
 }
 
