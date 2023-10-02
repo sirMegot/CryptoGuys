e20aeedd5dc799f175fb4d4abef8a02ea83d566a
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-10-02 13:59:27 +0200

    titfavdes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="votre-classe-de-mise-en-page">
-      <Component {...pageProps} />
-    </div>
-  );
-}
+    return (
+      <>
+        <Head>
+          <title>Crypto Guys</title>
+          <meta name="description" content="Welcome to Crypto Guys app !" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
+  }
 
 export default MyApp;
