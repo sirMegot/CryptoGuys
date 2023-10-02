42ea48d63444693d1abb6bff3065d910a20d6300
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-10-02 13:45:22 +0200

    ddt

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="votre-classe-de-mise-en-page">
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
