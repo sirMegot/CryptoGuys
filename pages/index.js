b45faeadf9d9b3e35b6c2db7ec057868e32d05b5
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-10-02 13:33:21 +0200

    popo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Web3 from 'web3';
-import Image from 'next/image'
+import Image from 'next/image';
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Crypto Guys',
+  description: 'Welcome to Crypto Guys app !',
+};
+
+function RootLayout({ children }) {
+  return <div className={inter.className}>{children}</div>;
+}
 
 function Home() {
   function connectToMetaMask() {
@@ -22,14 +35,16 @@ function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Crypto Guys -- &nbsp;
-          <button onClick={connectToMetaMask} className="font-mono font-bold">Connecter MetaMask</button>
-        </p>
-      </div>
-    </main>
+    <RootLayout>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Crypto Guys -- &nbsp;
+            <button onClick={connectToMetaMask} className="font-mono font-bold">Connecter MetaMask</button>
+          </p>
+        </div>
+      </main>
+    </RootLayout>
   );
 }
 
