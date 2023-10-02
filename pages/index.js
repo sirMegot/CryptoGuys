041ea0d6af7906f27014b8e9fe8013208d518828
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-10-02 13:25:35 +0200

    merge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Web3 from 'web3';
+import Image from 'next/image'
 
 function Home() {
   function connectToMetaMask() {
@@ -21,9 +22,14 @@ function Home() {
   }
 
   return (
-    <div>
-      <button onClick={connectToMetaMask}>Connecter MetaMask</button>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Crypto Guys -- &nbsp;
+          <button onClick={connectToMetaMask} className="font-mono font-bold">Connecter MetaMask</button>
+        </p>
+      </div>
+    </main>
   );
 }
 
